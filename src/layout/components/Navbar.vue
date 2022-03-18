371d724afccb4efe891f8358df67f5b604f9b6df
陈鹏飞 <913910173@qq.com>
2022-03-18 21:18:49 +0800

    修复关于打开修改文章页面自动下滑顶部出现错位的bug

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -62,7 +62,7 @@ export default {
 <style lang="scss" scoped>
 .navbar {
   height: 50px;
-  overflow: hidden;
+  // overflow: hidden;
   position: relative;
   background: #fff;
   box-shadow: 0 1px 4px rgba(0,21,41,.08);
